72a9a74c82598e9df69efc67283db9570b5160a0
unknown <vaibhav.guptaA@NDADEVPC152.markit.partners>
2014-09-09 19:35:51 +0530

    Seceond1

--- signups/views.py
+++ signups/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,render_to_response, RequestContext
+from django.contrib import messages
 # Create your views here.
+
+from .forms import SignUpForm
+def home(request):
+    form = SignUpForm(request.POST or None)    
+    if form.is_valid():
+        save_it = form.save(commit=False)
+        messages.success(request, 'Thanks Profile details updated...')
+        save_it.save()
+        
+        
+    return render_to_response("signup.html",locals(),context_instance=RequestContext(request))
