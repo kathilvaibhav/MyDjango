a923a46ade6624fe830265d57f3f9dd105b6586b
unknown <vaibhav.guptaA@NDADEVPC152.markit.partners>
2014-09-09 11:23:37 +0530

    Seceond

--- signups/models.py
+++ signups/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
 # Create your models here.
+from django.utils.encoding import smart_unicode
+class SignUp(models.Model):
+    first_name = models.CharField(max_length=120,null=True,blank=True)
+    last_name = models.CharField(max_length=120,null=True,blank=True)
+    email=models.EmailField()
+    
+    def __unicode__(self):
+        return smart_unicode(self.email)
